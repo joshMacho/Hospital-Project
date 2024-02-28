6c0c32b23c65eb935c6ea5568395908b12a484db
joshMacho <machomonei@gmail.com>
2024-02-28 13:18:11 +0000

    changes for a new hosting platform

--- src/components/apibase.js
+++ src/components/apibase.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://138.68.161.4:8080/api";
+//export const API_BASE_URL = "http://138.68.161.4:8080/api";
+export const API_BASE_URL = "https://hos-api-pmhk.onrender.com/api";
